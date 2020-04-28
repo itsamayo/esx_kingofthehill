927d1cc5f7c547ea6bf6747298d0be98e40b1d7e
Ashley Sanders <ashleymichaelsanders@gmail.com>
2020-04-27 23:29:05 -0500

    changed item reward to marijuana and added to SQL

--- db.sql
+++ db.sql
@@ -5,3 +5,6 @@ CREATE TABLE `payroll` (
 CREATE TABLE `payroll_code` (	
 	`code` VARCHAR(255) NOT NULL
 );
+
+INSERT INTO `items`(`name`, `label`, `limit`, `rare`,  `can_remove`) VALUES 
+('marijuana','Marijuana', 50, 0, 1);
